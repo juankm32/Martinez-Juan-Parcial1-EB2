5c554c835315d280155d81856cac02fb1bdafd9d
juankm32 <juank-m32@hotmail.com>
2023-08-24 09:53:30 -0500

    Configuraciones Seguridad con Keycloak

---
+++ ms-gateway/src/main/java/com/digitalhouse/msgateway/configuration/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.digitalhouse.msgateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class SecurityConfig {
+    private final ReactiveClientRegistrationRepository reactiveClientRegistrationRepository;
+
+    public SecurityConfig(ReactiveClientRegistrationRepository reactiveClientRegistrationRepository) {
+        this.reactiveClientRegistrationRepository = reactiveClientRegistrationRepository;
+    }
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain (ServerHttpSecurity http) {
+
+        http
+                .authorizeExchange()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .oauth2Login()
+                .and()
+                .logout()
+                .logoutSuccessHandler(oidcServerLogoutSuccessHandler());
+
+
+
+       /* http
+                .authorizeExchange()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .oauth2Login() // to redirect to oauth2 login page.
+                .and()
+                .logout()
+                .logoutSuccessHandler(oidcServerLogoutSuccessHandler());*/
+
+
+        return http.build();
+    }
+
+    private ServerLogoutSuccessHandler oidcServerLogoutSuccessHandler() {
+        OidcClientInitiatedServerLogoutSuccessHandler oidcClientInitiatedLogoutSuccessHandler
+                = new OidcClientInitiatedServerLogoutSuccessHandler(reactiveClientRegistrationRepository);
+        oidcClientInitiatedLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/login");
+        return oidcClientInitiatedLogoutSuccessHandler;
+    }
+}
